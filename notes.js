d9ae42d5323c3705fb081a963dd15d5c26f55ad3
hslape <136274044+hslape@users.noreply.github.com>
2023-09-18 20:01:24 -0500

    Add files via upload

---
+++ notes.js
@@ -0,0 +1,10 @@
+// Exercise 2
+
+// Objective - Familiarize with DOM events and inline event handlers in web development
+
+// Instructions:
+
+// 1. Create a new HTML file named dom.html and include a button with the class btn and btn_inline.
+// 2. Add an onclick event to this button that will call a JS function named changeBackground().
+// 3. Within the same HTML file, create a <script> section. Inside this section, define the changeBackground() function. When executed, this function should change the background color of the document.
+// Test you code by opening the dom.html in a browser and clicking the button. Did the background color change? If not, debug your code.
